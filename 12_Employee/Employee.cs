25f6fb37e814dfabba9bc061fc9f844f5d9e40b0
alex855k <alexander2341@gmail.com>
2016-10-05 15:55:07 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace _12_Employee
+{
+    internal class Employee
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string Type { get; internal set; }
+    }
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12_Employee
+{
+    internal class EmployeeRepository
+    {
+        internal void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int CountEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Employee CreateEmployee(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveEmployee(Employee simon)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Employee LoadEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<Employee> FindAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
